f5ad2728056153b8c4cfb5451c524e9976efdc5d
jml123 <langjinming18@gmail.com>
2026-04-14 10:28:55 +0800

    🚀 Add new resources: Wind-water and Documentaries

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,14 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import SocialShare from './components/SocialShare.vue'
+import ZiWeiCalculator from './components/ZiWeiCalculator.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('ZiWeiCalculator', ZiWeiCalculator)
+  },
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'nav-bar-content-after': () => h(SocialShare)
