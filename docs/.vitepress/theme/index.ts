d8f7d9ff8bd79c3cfdd51e04981da0b4a4335892
jml123 <langjinming18@gmail.com>
2026-04-20 10:23:52 +0800

    feat: add visitor stats (busuanzi) to site footer

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,6 +4,22 @@ import SocialShare from './components/SocialShare.vue'
 import ZiWeiCalculator from './components/ZiWeiCalculator.vue'
 import './custom.css'
 
+const VisitorStats = {
+  setup() {
+    return () => h('div', { class: 'visitor-stats', innerHTML: `
+      <span id="busuanzi_container_site_pv" style="display:none;">
+        🔥 本站总访问量 <span id="busuanzi_value_site_pv"></span> 次
+      </span>
+      <span id="busuanzi_container_site_uv" style="display:none;">
+        👥 本站访客数 <span id="busuanzi_value_site_uv"></span> 人
+      </span>
+      <span id="busuanzi_container_page_pv" style="display:none;">
+        📄 本文阅读量 <span id="busuanzi_value_page_pv"></span> 次
+      </span>
+    ` })
+  }
+}
+
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
@@ -11,7 +27,8 @@ export default {
   },
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'nav-bar-content-after': () => h(SocialShare)
+      'nav-bar-content-after': () => h(SocialShare),
+      'layout-bottom': () => h(VisitorStats)
     })
   }
 }
